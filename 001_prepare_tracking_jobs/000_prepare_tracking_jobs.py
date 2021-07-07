81357bfc3c9b47bff4eb378c71a47ca08d332425
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-07-07 14:31:23 +0200

    Closed orbit search and check

--- 001_prepare_tracking_jobs/000_prepare_tracking_jobs.py
+++ 001_prepare_tracking_jobs/000_prepare_tracking_jobs.py
@@ -1,13 +1,52 @@
+import pickle
+
+import numpy as np
+from scipy.optimize import fsolve
+
 import xline as xl
 import xtrack as xt
 
-line = xl.Line.from_json('../000_machine_model/xline/line_bb_dipole_not_cancelled.json')
+with open('../000_machine_model/optics_orbit_at_start_ring.pkl', 'rb') as fid:
+    ddd = pickle.load(fid)
+p0c = ddd['p0c_eV']
+
+line = xl.Line.from_json(
+        '../000_machine_model/xline/line_bb_dipole_not_cancelled.json')
+
+line_bb_off = line.copy()
+line_bb_off.disable_beambeam()
+
+tracker = xt.Tracker(sequence=line_bb_off)
+
+def one_turn_map(p):
+    part = xt.Particles(
+            p0c = p0c,
+            x = p[0],
+            px = p[1],
+            y = p[2],
+            py = p[3],
+            zeta = p[4],
+            delta = p[5])
+    tracker.track(part)
+    p_res = np.array([
+           part.x[0],
+           part.px[0],
+           part.y[0],
+           part.py[0],
+           part.zeta[0],
+           part.delta[0]])
+    return p_res
 
 
+print('Start CO search')
+res = fsolve(lambda p: p - one_turn_map(p), x0=np.array(6*[0]))
+print('Done CO search')
 
+part_co_dict = {'p0c': p0c, 'x': res[0], 'px': res[1], 'y': res[2], 'py': res[3],
+                'zeta': res[4], 'delta': res[5]}
 
-tracker = xt.Tracker(sequence=line)
-
-
-
-  
+particles = xt.Particles(**part_co_dict)
+for _ in range(10):
+    print(particles.at_turn[0], particles.x[0], particles.y[0],
+          particles.zeta[0])
+    tracker.track(particles)
