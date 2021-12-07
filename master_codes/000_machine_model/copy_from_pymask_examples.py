32462a986366a8a4e5ab0b2f828ac145f626e92f
Sofia Kostoglou <skostogl@hpc-201-11-01-a.cr.cnaf.infn.it>
2021-12-07 10:11:13 +0100

    avoid duplication

--- master_codes/000_machine_model/copy_from_pymask_examples.py
+++ master_codes/000_machine_model/copy_from_pymask_examples.py
@@ -4,7 +4,8 @@ import pymask
 example_folder = pymask._pkg_root.parent.joinpath(
    'python_examples/hl_lhc_collisions_python/')
 
-files_to_copy = ['000_pymask.py', 'optics_specific_tools.py']#, 'config.yaml']
+files_to_copy = ['000_pymask.py',
+                'optics_specific_tools.py']#, 'config.yaml']
 
 destination_path = '.'
 
