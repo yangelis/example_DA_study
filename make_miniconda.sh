441cf78f228b7f1d5554276d4d6b2611807d846c
sterbini <guido.sterbini@cern.ch>
2022-05-18 18:11:14 +0200

    Consolidating installation

--- make_miniconda.sh
+++ make_miniconda.sh
@@ -2,3 +2,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b  -p ./miniconda -f
 source miniconda/bin/activate
 python -m pip install -r requirements.txt
+git clone -b release/v0.1.0 git@github.com:xsuite/tree_maker.git
+python -m pip install -e tree_maker
