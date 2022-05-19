cd29439078832da2f94e650b12bc2444e9f6450f
sterbini <guido.sterbini@cern.ch>
2022-05-19 12:32:59 +0200

    Adding some dependences for the mask

--- make_miniconda.sh
+++ make_miniconda.sh
@@ -6,6 +6,8 @@ git clone -b release/v0.1.0 git@github.com:xsuite/tree_maker.git
 python -m pip install -e tree_maker
 git clone git@github.com:lhcopt/lhcmask.git
 python -m pip install -e lhcmask
+git clone git@github.com:lhcopt/lhcerrors.git
+git clone git@github.com:lhcopt/lhctoolkit.git
 cd master_study/master_jobs/001_machine_model/
 python 001_copy_from_pymask_examples.py
 cd ../../../
