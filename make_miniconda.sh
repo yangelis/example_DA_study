b7198715e2e2142da6ebabf6b579dbab5c7e023c
Colas Droin <colasdroin@gmail.com>
2023-05-08 17:34:07 +0200

    Updated make_miniconda

--- make_miniconda.sh
+++ make_miniconda.sh
@@ -2,14 +2,12 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b  -p ./miniconda -f
 source miniconda/bin/activate
 python -m pip install -r requirements.txt
+mkdir modules
+cd modules
 git clone -b release/v0.1.0 git@github.com:xsuite/tree_maker.git
 python -m pip install -e tree_maker
-git clone git@github.com:lhcopt/lhcmask.git
-python -m pip install -e lhcmask
-git clone git@github.com:lhcopt/lhcerrors.git
-git clone git@github.com:lhcopt/lhctoolkit.git
-git clone git@github.com:lhcopt/hllhc15.git
-git clone $(whoami)@lxplus.cern.ch:/afs/cern.ch/eng/lhc/optics/runIII
+# git clone https://github.com/PyCOMPLETE/FillingPatterns.git
+# python -m pip install ./FillingPatterns
 cd master_study/master_jobs/001_machine_model/
 python 001_copy_from_pymask_examples.py
 cd ../../../
