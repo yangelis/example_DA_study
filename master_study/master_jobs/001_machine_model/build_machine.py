04903c50ec4694ca037af4a52a7c5fa5c5f88a03
Colas Droin <colasdroin@gmail.com>
2023-05-09 11:29:02 +0200

    Minor fix

--- master_study/master_jobs/001_machine_model/build_machine.py
+++ master_study/master_jobs/001_machine_model/build_machine.py
@@ -36,7 +36,7 @@ ost.build_sequence(mad_b4, mylhcbeam=4)
 ost.apply_optics(mad_b1b2, optics_file=config_mad_model["optics_file"])
 
 # Build xsuite collider
-collider = xmlhc.build_xsuite_collider(
+collider = xlhc.build_xsuite_collider(
     sequence_b1=mad_b1b2.sequence.lhcb1,
     sequence_b2=mad_b1b2.sequence.lhcb2,
     sequence_b4=mad_b4.sequence.lhcb2,
@@ -49,8 +49,6 @@ collider = xmlhc.build_xsuite_collider(
     ver_hllhc_optics=config_mad_model["ver_hllhc_optics"],
 )
 
-# Save to file
-# collider.to_json("xsuite_lines/collider_00_from_mad.json")
 
 # Remove all the temporaty files created in the process of building collider
 os.remove("mad_collider.log")
