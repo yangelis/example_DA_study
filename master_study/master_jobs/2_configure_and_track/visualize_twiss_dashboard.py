7daca9598868a78799e51eb022b9947a80caf6a9
Colas Droin <colasdroin@gmail.com>
2023-06-05 15:03:25 +0200

    Update for sanity checks

--- master_study/master_jobs/2_configure_and_track/visualize_twiss_dashboard.py
+++ master_study/master_jobs/2_configure_and_track/visualize_twiss_dashboard.py
@@ -10,6 +10,7 @@ import base64
 import xtrack as xt
 import io
 import json
+import yaml
 
 # Import functions
 import dashboard_functions
@@ -20,7 +21,30 @@ dashboard_functions.build_CSS()
 
 #################### Load global variables ####################
 
+# Path to configuration file
 path_configuration = "/afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/scans/opt_flathv_75_1500_withBB_chroma5_1p4_eol_bunch_scan/base_collider/xtrack_0002/config.yaml"
+
+# Load configuration file
+with open(path_configuration, "r") as fid:
+    configuration = yaml.safe_load(fid)["config_collider"]
+    num_particles_per_bunch = float(configuration["config_beambeam"]["num_particles_per_bunch"])
+    nemitt_x = configuration["config_beambeam"]["nemitt_x"] * 1e-6
+    nemitt_y = configuration["config_beambeam"]["nemitt_y"] * 1e-6
+    sigma_z = configuration["config_beambeam"]["sigma_z"]
+
+# Load the filling scheme
+path_filling_scheme = "/afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/master_jobs/filling_scheme/8b4e_1972b_1960_1178_1886_224bpi_12inj_800ns_bs200ns.json"
+with open(path_filling_scheme) as fid:
+    filling_scheme = json.load(fid)
+
+array_b1 = np.array(filling_scheme["beam1"])
+array_b2 = np.array(filling_scheme["beam2"])
+
+# Assert that the arrays have the required length, and do the convolution
+assert len(array_b1) == len(array_b2) == 3564
+n_collisions_ip1_and_5 = array_b1 @ array_b2
+n_collisions_ip2 = np.roll(array_b1, -891) @ array_b2
+n_collisions_ip8 = np.roll(array_b1, -2670) @ array_b2
 
 # Get collider variables
 collider, tw_b1, df_sv_b1, df_tw_b1, tw_b2, df_sv_b2, df_tw_b2, df_elements_corrected = (
@@ -35,14 +59,6 @@ table_tw_b1 = dashboard_functions.return_data_table(df_tw_b1, "id-df-tw-b1", twi
 table_sv_b2 = dashboard_functions.return_data_table(df_sv_b2, "id-df-sv-b2", twiss=False)
 table_tw_b2 = dashboard_functions.return_data_table(df_tw_b2, "id-df-tw-b2", twiss=True)
 
-# Get filling scheme
-path_filling_scheme = "/afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/master_jobs/filling_scheme/8b4e_1972b_1960_1178_1886_224bpi_12inj_800ns_bs200ns.json"
-
-with open(path_filling_scheme) as fid:
-    filling_scheme = json.load(fid)
-
-array_b1 = np.array(filling_scheme["beam1"])
-array_b2 = np.array(filling_scheme["beam2"])
 
 #################### App ####################
 app = Dash(
@@ -99,6 +115,7 @@ def return_header_layout():
                                 {"value": "display-configuration", "label": "Configuration"},
                                 {"value": "display-twiss", "label": "Twiss tables"},
                                 {"value": "display-scheme", "label": "Filling scheme"},
+                                {"value": "display-sanity", "label": "Sanity checks"},
                                 {"value": "display-optics", "label": "Optics"},
                                 {"value": "display-survey", "label": "Survey"},
                             ],
@@ -176,9 +193,199 @@ def return_tables_layout():
             ),
             html.Div(id="placeholder-data-table"),
         ],
-        style={"width": "100%", "margin": "auto"},
+        style={"width": "90%", "margin": "auto"},
     )
     return layout
+
+
+def return_sanity_layout():
+    # Check general observables (tune, chroma, etc.)
+    header_1 = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th("Beam"),
+                    html.Th("Tune"),
+                    html.Th("Chromaticity"),
+                    html.Th("Linear coupling"),
+                    html.Th("Momentum compaction factor"),
+                ]
+            )
+        )
+    ]
+
+    row1 = html.Tr(
+        [
+            html.Td("1"),
+            html.Td(f'{tw_b1["qx"]:.5f}'),
+            html.Td(f'{tw_b1["dqx"]:.2f}'),
+            html.Td(f'{tw_b1["c_minus"]:.2f}'),
+            html.Td(f'{tw_b1["momentum_compaction_factor"]:.2f}'),
+        ]
+    )
+    row2 = html.Tr(
+        [
+            html.Td("2"),
+            html.Td(f'{tw_b2["qx"]:.5f}'),
+            html.Td(f'{tw_b2["dqx"]:.2f}'),
+            html.Td(f'{tw_b2["c_minus"]:.2f}'),
+            html.Td(f'{tw_b2["momentum_compaction_factor"]:.2f}'),
+        ]
+    )
+    body_1 = [html.Tbody([row1, row2])]
+    table_1 = dmc.Table(header_1 + body_1)
+
+    # Check IP-specific observables (crossing angle, beta functions, etc.)
+    header_2 = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th("IP"),
+                    html.Th("s"),
+                    html.Th("x"),
+                    html.Th("px"),
+                    html.Th("y"),
+                    html.Th("py"),
+                    html.Th("betx"),
+                    html.Th("bety"),
+                ]
+            )
+        )
+    ]
+    l_rows_1 = []
+    l_rows_2 = []
+    for ip in [1, 2, 5, 8]:
+        row_values_1 = (
+            tw_b1.rows[f"ip{ip}"]
+            .cols["s", "x", "px", "y", "py", "betx", "bety"]
+            .to_pandas()
+            .to_numpy()
+            .squeeze()
+        )
+
+        row_values_2 = (
+            tw_b2.rows[f"ip{ip}"]
+            .cols["s", "x", "px", "y", "py", "betx", "bety"]
+            .to_pandas()
+            .to_numpy()
+            .squeeze()
+        )
+
+        l_rows_1.append(
+            html.Tr(
+                [
+                    html.Td(row_values_1[0]),
+                    html.Td(f"{row_values_1[1]:.3f}"),
+                    html.Td(f"{row_values_1[2]:.4f}"),
+                    html.Td(f"{row_values_1[3]:.5f}"),
+                    html.Td(f"{row_values_1[4]:.4f}"),
+                    html.Td(f"{row_values_1[5]:.5f}"),
+                    html.Td(f"{row_values_1[6]:.3f}"),
+                    html.Td(f"{row_values_1[7]:.3f}"),
+                ]
+            )
+        )
+
+        l_rows_2.append(
+            html.Tr(
+                [
+                    html.Td(row_values_2[0]),
+                    html.Td(f"{row_values_2[1]:.3f}"),
+                    html.Td(f"{row_values_2[2]:.4f}"),
+                    html.Td(f"{row_values_2[3]:.5f}"),
+                    html.Td(f"{row_values_2[4]:.4f}"),
+                    html.Td(f"{row_values_2[5]:.5f}"),
+                    html.Td(f"{row_values_2[6]:.3f}"),
+                    html.Td(f"{row_values_2[7]:.3f}"),
+                ]
+            )
+        )
+
+    body_2 = [html.Tbody(l_rows_1)]
+    body_3 = [html.Tbody(l_rows_2)]
+    table_2 = dmc.Table(header_2 + body_2)
+    table_3 = dmc.Table(header_2 + body_3)
+
+    # Luminosities
+    header_3 = [
+        html.Thead(
+            html.Tr(
+                [
+                    html.Th("IP 1"),
+                    html.Th("IP 2"),
+                    html.Th("IP 5"),
+                    html.Th("IP 8"),
+                ]
+            )
+        )
+    ]
+
+    l_lumi = []
+    for ip, n_col in zip(
+        [1, 2, 5, 8],
+        [n_collisions_ip1_and_5, n_collisions_ip2, n_collisions_ip1_and_5, n_collisions_ip8],
+    ):
+        l_lumi.append(
+            xt.lumi.luminosity_from_twiss(
+                n_colliding_bunches=n_col,
+                num_particles_per_bunch=num_particles_per_bunch,
+                ip_name="ip" + str(ip),
+                nemitt_x=nemitt_x,
+                nemitt_y=nemitt_y,
+                sigma_z=sigma_z,
+                twiss_b1=tw_b1,
+                twiss_b2=tw_b2,
+                crab=False,
+            )
+        )
+    row_lumi = html.Tr(
+        [
+            html.Td(f"{l_lumi[0]:.3e}"),
+            html.Td(f"{l_lumi[1]:.3e}"),
+            html.Td(f"{l_lumi[2]:.3e}"),
+            html.Td(f"{l_lumi[3]:.3e}"),
+        ]
+    )
+    body_4 = [html.Tbody([row_lumi])]
+    table_4 = dmc.Table(header_3 + body_4)
+
+    return dmc.Stack(
+        children=[
+            dmc.Group(
+                children=[
+                    dmc.Text("General observables", size="xl", style={"margin": "auto"}),
+                    table_1,
+                ],
+                mb=10,
+                style={"width": "100%"},
+            ),
+            dmc.Group(
+                children=[
+                    dmc.Text("Beam 1 observables at IPs", size="xl", style={"margin": "auto"}),
+                    table_2,
+                ],
+                mb=10,
+                style={"width": "100%"},
+            ),
+            dmc.Group(
+                children=[
+                    dmc.Text("Beam 2 observables at IPs", size="xl", style={"margin": "auto"}),
+                    table_3,
+                ],
+                mb=10,
+                style={"width": "100%"},
+            ),
+            dmc.Group(
+                children=[
+                    dmc.Text("Luminosities", size="xl", style={"margin": "auto"}),
+                    table_4,
+                ],
+                mb=10,
+                style={"width": "100%"},
+            ),
+        ],
+        style={"width": "90%", "margin": "auto"},
+    )
 
 
 def return_survey_layout():
@@ -349,24 +556,6 @@ layout = html.Div(
                     style={"width": "100%", "margin": "auto"},
                     children=[
                         html.Div(id="placeholder-tabs"),
-                        # dmc.TabsPanel(
-                        #     children=return_configuration_layout(path_configuration),
-                        #     value="display-configuration",
-                        # ),
-                        # dmc.TabsPanel(
-                        #     children=return_tables_layout(),
-                        #     value="display-twiss",
-                        #     style={"height": "90vh", "width": "80%", "margin": "auto"},
-                        # ),
-                        # dmc.TabsPanel(
-                        #     children=return_filling_scheme_layout(),
-                        #     value="display-scheme",
-                        # ),
-                        # dmc.TabsPanel(children=return_optics_layout(), value="display-optics"),
-                        # dmc.TabsPanel(
-                        #     children=return_survey_layout(),
-                        #     value="display-survey",
-                        # ),
                     ],
                 ),
             ],
@@ -397,6 +586,8 @@ def select_tab(value):
             return return_tables_layout()
         case "display-scheme":
             return return_filling_scheme_layout()
+        case "display-sanity":
+            return return_sanity_layout()
         case "display-optics":
             return return_optics_layout()
         case "display-survey":
