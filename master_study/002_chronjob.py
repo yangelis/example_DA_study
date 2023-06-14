0dbd206b81e2d001efbfc36cdf5b0ec5a4d9d3dc
Colas Droin <colasdroin@gmail.com>
2023-06-14 15:40:20 +0200

    New studies hybrid

--- master_study/002_chronjob.py
+++ master_study/002_chronjob.py
@@ -124,7 +124,9 @@ class cluster:
 # ==================================================================================================
 # Load the tree from a yaml
 if __name__ == "__main__":
-    study_name = "opt_flathv_75_1500_withBB_chroma15_eol_tune_tune_2228"  # example_HL_tunescan"
+    study_name = (  # example_HL_tunescan"
+        "opt_flathv_500_1000_withBB_chroma15_2p3_sol_tune_tune_2228"
+    )
     fix = "/scans/" + study_name
     root = tree_maker.tree_from_json(fix[1:] + "/tree_maker_" + study_name + ".json")
     # Add suffix to the root node path to handle scans that are not in the root directory
