9ffb038abb493fc7a1d7dcbc0f0b726b7b473ff9
Colas Droin <colasdroin@gmail.com>
2023-06-28 10:53:54 +0200

    Fixed bug master job 2024

--- master_study/run_2024/1_build_distr_and_collider/1_build_distr_and_collider.py
+++ master_study/run_2024/1_build_distr_and_collider/1_build_distr_and_collider.py
@@ -157,7 +157,7 @@ def build_distr_and_collider(config_file="config.yaml"):
 
     # Save collider to json
     os.makedirs("collider", exist_ok=True)
-    collider.to_json("collider/collider_thick.json")
+    collider.to_json("collider/collider.json")
 
     # Tag end of the job
     tree_maker_tagging(configuration, tag="completed")
