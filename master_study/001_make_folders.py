b787f070c7b3cdf2a4bc7d0f239e5df58fa99c95
Colas <colasdroin@gmail.com>
2023-08-17 17:40:56 +0200

    Update make_folders

--- master_study/001_make_folders.py
+++ master_study/001_make_folders.py
@@ -55,7 +55,7 @@ d_config_mad = {"beam_config": {"lhcb1": {}, "lhcb2": {}}, "links": {}}
 
 ### For v1.6 optics
 d_config_mad["links"]["acc-models-lhc"] = "../../../../modules/hllhc16"
-d_config_mad["optics_file"] = "acc-models-lhc/strengths/flat/opt_flathv_500_2000.madx"
+d_config_mad["optics_file"] = "acc-models-lhc/strengths/flat/opt_flathv_500_2000_thin.madx"
 d_config_mad["ver_hllhc_optics"] = 1.6
 
 
