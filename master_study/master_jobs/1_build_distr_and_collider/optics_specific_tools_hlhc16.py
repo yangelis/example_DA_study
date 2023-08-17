909715712536a94ab34039d5000b8c97020243ee
Guido Sterbini <guido.sterbini@cern.ch>
2023-08-17 16:01:03 +0200

    Status

--- master_study/master_jobs/1_build_distr_and_collider/optics_specific_tools_hlhc16.py
+++ master_study/master_jobs/1_build_distr_and_collider/optics_specific_tools_hlhc16.py
@@ -1,10 +1,37 @@
 from xmask.lhc import install_errors_placeholders_hllhc
+import numpy as np
 
+def check_madx_lattices(mad):
+    assert mad.globals['qxb1']== mad.globals['qxb2']
+    assert mad.globals['qyb1']== mad.globals['qyb2']
+    assert mad.globals['qpxb1']== mad.globals['qpxb2']
+    assert mad.globals['qpyb1']== mad.globals['qpyb2']
+
+    assert np.isclose(mad.table.summ.q1, mad.globals['qxb1'], atol=1e-05)
+    assert np.isclose(mad.table.summ.q2, mad.globals['qyb1'], atol=1e-05)
+    assert np.isclose(mad.table.summ.dq1, mad.globals['qpxb1'], atol=1e-03)
+    assert np.isclose(mad.table.summ.dq2, mad.globals['qpyb1'], atol=1e-03)
+
+    df =  mad.table.twiss.dframe()
+    for my_ip in [1,2,5,8]:
+      assert np.isclose(df.loc[f'ip{my_ip}'].betx, mad.globals[f'betx_IP{my_ip}'], rtol=1e-03)
+      assert np.isclose(df.loc[f'ip{my_ip}'].bety, mad.globals[f'bety_IP{my_ip}'], rtol=1e-03)
+    
+    assert df['x'].std() < 1e-8
+    assert df['y'].std() < 1e-8
+
+def check_xsuite_lattices(my_line):
+  tw = my_line.twiss(method='6d', matrix_stability_tol=100)
+  print(f"--- Now displaying Twiss result at all IPS for line {my_line}---")
+  print(tw[:, "ip.*"])
+  # print qx and qy
+  print(f"--- Now displaying Qx and Qy for line {my_line}---")
+  print(tw.qx, tw.qy)
 
 def build_sequence(
     mad,
     mylhcbeam,
-    optics_version=1.5,
+    optics_version=1.6,
     apply_fix_1p6=True,
     ignore_cycling=False,
     ignore_CC=False,
@@ -21,11 +48,11 @@ def build_sequence(
     else:
         raise ValueError("Optics version not supported")
 
-    mad.input(f"""
-      ! Get the toolkit
-      call,file=
-        "acc-models-lhc/toolkit/macro.madx";
-      """)
+    # mad.input(f"""
+    #   ! Get the toolkit
+    #   call,file=
+    #     "acc-models-lhc/toolkit/macro.madx";
+    #   """)
 
     mad.input(f"""
       ! Build sequence
@@ -35,6 +62,12 @@ def build_sequence(
       }} else {{
         call,file="acc-models-lhc{path}lhc.seq";
       }};
+      !Install HL-LHC
+      call, file=
+        "acc-models-lhc/hllhc_sequence.madx";
+      ! Get the toolkit
+      call,file=
+        "acc-models-lhc/toolkit/macro.madx";
       option, -echo, warn,-info;
       """)
 
@@ -62,16 +95,18 @@ def build_sequence(
         """)
 
     mad.input(
-        f"""
-      !Install HL-LHC
-      call, file=
-        "acc-models-lhc/hllhc_sequence.madx";
       """
-        """
       ! Slice nominal sequence
       exec, myslice;
       """
     )
+    if mylhcbeam<3:
+      mad.input(f"""
+      nrj=7000;
+      beam,particle=proton,sequence=lhcb1,energy=nrj,npart=1.15E11,sige=4.5e-4;
+      beam,particle=proton,sequence=lhcb2,energy=nrj,bv = -1,npart=1.15E11,sige=4.5e-4;
+      """)
+      
 
     install_errors_placeholders_hllhc(mad)
 
